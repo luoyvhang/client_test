b29c7eef0dcca0a55ef94e2bb13b091893c5d379
luoyvhang <576833228@qq.com>
2018-12-07 21:21:35 +0800

    ttttttttt

--- src/app/models/moduleid.lua
+++
@@ -1,6 +0,0 @@
-local netmodule = {
-  PLAYER = 1,
-  ROOM = 2
-}
-
-return netmodule
--- src/devconf.lua
+++
@@ -1,6 +0,0 @@
-return {
-    -- host = '192.168.0.164',
-    host = '47.92.201.15',
-    port = 28302,
-    STARTUP = 'LoginController',
-}
