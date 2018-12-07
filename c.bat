72df487543444d9ffd07c0aa6a744805b351b66c
luoyvhang <576833228@qq.com>
2018-12-07 12:40:23 +0800

    ttttttt

--- c.bat
+++
@@ -1,3 +0,0 @@
-cocos compile --android-studio --target android-21 --ap android-21 --ndk-mode release --ndk-toolchain arm-linux-androideabi-4.8  --platform android --app-abi armeabi --no-apk
-
-pause
